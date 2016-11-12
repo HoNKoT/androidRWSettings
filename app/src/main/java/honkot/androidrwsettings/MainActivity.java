dd3826d903964d35a6f7b6f64eab6d0772dac546
Hiroki.H <hon87kot12.xp@gmail.com>
2016-11-13 08:37:09 +0900

    Add write Automatic date&time (It’s permitted for System App)

--- app/src/main/java/honkot/androidrwsettings/MainActivity.java
+++ app/src/main/java/honkot/androidrwsettings/MainActivity.java
@@ -1,5 +1,6 @@
 package honkot.androidrwsettings;
 
+import android.os.Build;
 import android.provider.Settings;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         findViewById(R.id.read).setOnClickListener(this);
         findViewById(R.id.write).setOnClickListener(this);
+
+        TextView tv = (TextView)findViewById(R.id.result);
+        tv.setText("API level:" + Build.VERSION.SDK_INT);
     }
 
     @Override
@@ -24,20 +28,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         StringBuffer buf = new StringBuffer();
         switch (view.getId()) {
             case R.id.read:
-                buf.append("Auto Date&Time:");
+                buf.append("Get Auto Date&Time:");
                 buf.append(getAutoState());
                 break;
-            case R.id.write: break;
+            case R.id.write:
+                buf.append("Set Auto Date&Time: true");
+                setAutoState(true);
+                break;
         }
         tv.setText(buf.toString());
     }
 
     private boolean getAutoState() {
         try {
-            return Settings.System.getInt(getContentResolver(),
-                    Settings.System.AUTO_TIME) > 0;
+            if (Build.VERSION.SDK_INT < 17) {
+                return Settings.System.getInt(getContentResolver(),
+                        Settings.System.AUTO_TIME) > 0;
+            } else {
+                return android.provider.Settings.Global.getInt(
+                        getContentResolver(), android.provider.Settings.Global.AUTO_TIME, 0) > 0;
+            }
         } catch (Settings.SettingNotFoundException snfe) {
             return true;
+        }
+    }
+
+    private void setAutoState(boolean value) {
+        if (Build.VERSION.SDK_INT < 17) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.AUTO_TIME, value ? 1 : 0);
+        } else {
+            android.provider.Settings.Global.putInt(
+                    getContentResolver(), android.provider.Settings.Global.AUTO_TIME, value ? 1 : 0);
         }
     }
 }
