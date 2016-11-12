1311ba7b08c4f3113f6792a3d639aba347945e0d
Hiroki.H <hon87kot12.xp@gmail.com>
2016-11-13 08:16:30 +0900

    Add read Automatic date&time

--- app/src/main/java/honkot/androidrwsettings/MainActivity.java
+++ app/src/main/java/honkot/androidrwsettings/MainActivity.java
@@ -1,8 +1,11 @@
 package honkot.androidrwsettings;
 
+import android.provider.Settings;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,9 +20,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        TextView tv = (TextView)findViewById(R.id.result);
+        StringBuffer buf = new StringBuffer();
         switch (view.getId()) {
-            case R.id.read: break;
+            case R.id.read:
+                buf.append("Auto Date&Time:");
+                buf.append(getAutoState());
+                break;
             case R.id.write: break;
+        }
+        tv.setText(buf.toString());
+    }
+
+    private boolean getAutoState() {
+        try {
+            return Settings.System.getInt(getContentResolver(),
+                    Settings.System.AUTO_TIME) > 0;
+        } catch (Settings.SettingNotFoundException snfe) {
+            return true;
         }
     }
 }
